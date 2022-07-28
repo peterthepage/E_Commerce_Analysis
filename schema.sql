2695601088c0a40532832ff74058846ba3351f46
chriswilliamstrombone <chriswilliamstrombone@gmail.com>
2022-07-27 19:00:42 -0500

    updated ECommerce_October_Revised and schema

--- schema.sql
+++ schema.sql
@@ -1,25 +1,23 @@
 -- Creating tables for eCommerce behavior data
-CREATE TABLE Cleaned_ECommerce_Oct_2019 (
+CREATE TABLE purchase_Oct_2019 (
 	event_time Date,
-    event_type VARCHAR,
     product_id INT,
-    category_id INT,
     category_code VARCHAR,
     brand VARCHAR,
 	price DECIMAL,
 	
-     PRIMARY KEY (item_index)
+    PRIMARY KEY (item_index)
 );
 
 -- Creating tables for eCommerce behavior data
-CREATE TABLE Cleaned_ECommerce_Nov_2019 (
+CREATE TABLE cart_Oct_2019 (
 	event_time Date,
-    event_type VARCHAR,
     FOREIGN KEY (product_id) REFERENCES Cleaned_ECommerce_Oct_2019 (product_id),
-    category_id INT,
     category_code VARCHAR,
     brand VARCHAR,
 	price DECIMAL,
 	
 );
+
+
 
