9453e0fe5b9d32ec67b9c6e3f51fd559a539f040
chriswilliamstrombone <chriswilliamstrombone@gmail.com>
2022-07-30 16:39:36 -0500

    adding aws connection to Oct notebook, updated ERD

--- ERD.sql
+++ ERD.sql
@@ -1,4 +1,4 @@
-Cleaned_ECommerce_Oct_2019
+Cart_Oct_2019
 -
 event_time Date
 event_type VARCHAR
@@ -8,11 +8,31 @@ category_code VARCHAR
 brand VARCHAR
 price DECIMAL
 
-Cleaned_ECommerce_Nov_2019
+Purchase_Oct_2019
 -
 event_time Date
 event_type VARCHAR
-product_id INT FK - Cleaned_ECommerce_Oct_2019.product_id
+product_id INT FK - Cart_Oct_2019.product_id
+category_id INT
+category_code VARCHAR
+brand VARCHAR
+price DECIMAL
+
+Cart_Nov_2019
+-
+event_time Date
+event_type VARCHAR
+product_id INT FK - Cart_Oct_2019.product_id
+category_id INT
+category_code VARCHAR
+brand VARCHAR
+price DECIMAL
+
+Purchase_Nov_2019
+-
+event_time Date
+event_type VARCHAR
+product_id INT FK - Cart_Oct_2019.product_id
 category_id INT
 category_code VARCHAR
 brand VARCHAR
