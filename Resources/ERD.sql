ed066f9a39399316e42252b3d3871f68ee11fdaa
chriswilliamstrombone <chriswilliamstrombone@gmail.com>
2022-07-31 17:42:03 -0500

    adding index column to oct and nov notebooks, updated schema and ERD

--- Resources/ERD.sql
+++ Resources/ERD.sql
@@ -1,31 +1,35 @@
 Cart_Oct_2019
 -
+event_index INT PK
 event_time Date
-product_id INT PK
+product_id INT
 category_code VARCHAR
 brand VARCHAR
 price DECIMAL
 
 Purchase_Oct_2019
 -
+event_index INT FK - Cart_Oct_2019.event_index
 event_time Date
-product_id INT FK - Cart_Oct_2019.product_id
+product_id INT 
 category_code VARCHAR
 brand VARCHAR
 price DECIMAL
 
 Cart_Nov_2019
 -
+event_index INT FK - Cart_Oct_2019.event_index
 event_time Date
-product_id INT FK - Cart_Oct_2019.product_id
+product_id INT 
 category_code VARCHAR
 brand VARCHAR
 price DECIMAL
 
 Purchase_Nov_2019
 -
+event_index INT FK - Cart_Oct_2019.event_index
 event_time Date
-product_id INT FK - Cart_Oct_2019.product_id
+product_id INT 
 category_code VARCHAR
 brand VARCHAR
 price DECIMAL
--- Resources/schema.sql
+++ Resources/schema.sql
@@ -1,40 +1,45 @@
 -- Creating tables for eCommerce behavior data
 CREATE TABLE Cart_Oct_2019 (
-	event_time Date,
-    product_id INT,
-    category_code VARCHAR,
-    brand VARCHAR,
-	price DECIMAL,
-	
-    PRIMARY KEY (item_index)
+        event_index INT PRIMARY KEY,
+	    event_time TIMESTAMP,
+        product_id INT,
+        category_code VARCHAR,
+        brand VARCHAR,
+	    price DECIMAL
+
+
 );
 
 -- Creating tables for eCommerce behavior data
 CREATE TABLE Purchase_Oct_2019 (
-	event_time Date,
-    FOREIGN KEY (product_id) REFERENCES Cart_Oct_2019 (product_id),
-    category_code VARCHAR,
-    brand VARCHAR,
-	price DECIMAL,
-	
+	    event_index INT,
+        event_time TIMESTAMP,
+        product_id INT,
+        category_code VARCHAR,
+        brand VARCHAR,
+	    price DECIMAL
+
+        	
 );
 
 -- Creating tables for eCommerce behavior data
 CREATE TABLE Cart_Nov_2019 (
-	event_time Date,
-    FOREIGN KEY (product_id) REFERENCES Cart_Oct_2019 (product_id),
-    category_code VARCHAR,
-    brand VARCHAR,
-	price DECIMAL,
-	
+        event_index INT,
+	    event_time TIMESTAMP,
+        product_id INT,
+        category_code VARCHAR,
+        brand VARCHAR,
+	    price DECIMAL
+        
 );
 
 -- Creating tables for eCommerce behavior data
 CREATE TABLE Purchase_Nov_2019 (
-	event_time Date,
-    FOREIGN KEY (product_id) REFERENCES Cart_Oct_2019 (product_id),
-    category_code VARCHAR,
-    brand VARCHAR,
-	price DECIMAL,
-	
+        event_index INT,
+	    event_time TIMESTAMP,
+        product_id INT,
+        category_code VARCHAR,
+        brand VARCHAR,
+	    price DECIMAL
+
 );
