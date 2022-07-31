c56e652e7ecc5ae1e13cb5f23d25846dfbe9ea05
chriswilliamstrombone <chriswilliamstrombone@gmail.com>
2022-07-31 17:42:55 -0500

    updating query

--- Resources/E_Commerce_Database_Query.sql
+++ Resources/E_Commerce_Database_Query.sql
@@ -1,17 +1,15 @@
-
-
-alter table Purchase_Oct_2019
-add constraint FK_event_time
-foreign key (event_time)
-references Cart_Oct_2019(event_time);
-
 alter table Cart_Nov_2019
 add constraint FK_event_time
-foreign key (event_time)
-references Cart_Oct_2019(event_time);
+FOREIGN key (event_index)
+REFERENCES cart_oct_2019(event_index);
 
 alter table Purchase_Nov_2019
 add constraint FK_event_time
-foreign key (event_time)
-references Cart_Oct_2019(event_time);
+FOREIGN key (event_index)
+REFERENCES cart_oct_2019(event_index);
+
+alter table Purchase_Oct_2019
+add constraint FK_event_time
+FOREIGN key (event_index)
+REFERENCES cart_oct_2019(event_index);
 
