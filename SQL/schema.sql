1ad0fe2fb9da2475d2a1e2bd308bcac7ceb3e97f
chriswilliamstrombone <chriswilliamstrombone@gmail.com>
2022-08-07 12:55:10 -0500

    adding notebook to reduces data for pg Admin, adding SQL folder

--- SQL/schema.sql
+++ SQL/schema.sql
@@ -64,3 +64,52 @@ CREATE TABLE Purchase_Nov_2019 (
 	price DECIMAL
 
 );
+
+-- Creating tables for eCommerce behavior data
+CREATE TABLE top_five_oct_2019 (
+        
+        event_index INT,
+	event_time TIMESTAMP,
+        product_id INT,
+        category_code VARCHAR,
+        brand VARCHAR,
+	price DECIMAL
+
+);
+
+-- Creating tables for eCommerce behavior data
+CREATE TABLE top_five_nov_2019 (
+        
+        event_index INT,
+	event_time TIMESTAMP,
+        product_id INT,
+        category_code VARCHAR,
+        brand VARCHAR,
+	price DECIMAL
+
+);
+
+-- Creating tables for eCommerce behavior data
+CREATE TABLE washers_oct_2019 (
+        
+        event_index INT,
+	event_time TIMESTAMP,
+        product_id INT,
+        category_code VARCHAR,
+        brand VARCHAR,
+	price DECIMAL
+
+);
+
+-- Creating tables for eCommerce behavior data
+CREATE TABLE washers_nov_2019 (
+        
+        event_index INT,
+	event_time TIMESTAMP,
+        product_id INT,
+        category_code VARCHAR,
+        brand VARCHAR,
+	price DECIMAL
+
+);
+
